24d2f5ad8dc7e9c5f8df2e0028b71850eb176140
Samy Waltersam <office@goodcare.at>
2020-11-08 18:11:10 +0100

    added Firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD9irIksvVFGkjwIz08aEsBz4nVepItyF0",
+    authDomain: "goodcare-a8724.firebaseapp.com",
+    databaseURL: "https://goodcare-a8724.firebaseio.com",
+    projectId: "goodcare-a8724",
+    storageBucket: "goodcare-a8724.appspot.com",
+    messagingSenderId: "847502315439",
+    appId: "1:847502315439:web:a417fee532a70b4ac5295a",
+    measurementId: "G-57RRRRV6WM"
+  }
 };
 
 /*
