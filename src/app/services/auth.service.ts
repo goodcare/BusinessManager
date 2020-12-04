5ed6e3cece14d5af4e7b07f123723d83c0e4d9b0
Samy Waltersam <office@goodcare.at>
2020-12-04 17:11:14 +0100

    removed auth.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+// import { auth } from 'firebase/app'
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { switchMap, take, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { DbService } from './db.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user$: Observable<any>;
 
   constructor(
     private afAuth: AngularFireAuth,
     private db: DbService,
     private router: Router
-  ) { }
+  ) {
+    //this.user$ = this.afAuth.authState.pipe(
+    //  switchMap(user => (user ? db.doc$(`users/${user.uid}`) : of(null)))
+    //);
+   }
 }
